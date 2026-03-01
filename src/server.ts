feaf079fe7f9814da8ad583d687666dd5d3f7b51
Harish Puri <harishpuri.in@gmail.com>
2026-03-01 22:23:28 +0530

    Server created and tested

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import { Request, Response } from 'express';
 
 const app = express();
 const PORT = 3000;
+
+app.use('/', (req: Request, res: Response) => {
+    console.log("Hey there")
+    return res.json({"harish": "puri"})
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`)
